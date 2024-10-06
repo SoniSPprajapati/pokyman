606f1a02f7b1388eecd5c246b86df5fe62bc8bc3
SoniSPprajapati <prajapatisoni839@gmail.com>
2024-10-06 09:23:10 +0000

    Final code

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,40 @@
-// alert("Hi");
+import data from "./data.json";
+// Dom Targeting
+const cardRow = document.querySelector("#card-row");
 const inputEl = document.querySelector("input");
+
+for (let pokomonobj of data) {
+  const div = document.createElement("div");
+  div.classList.add("col");
+
+  div.innerHTML = `
+          <div class="card">
+            <img
+              src="${pokomonobj.image}"
+              class="card-img-top"
+              alt="..."
+            />
+            <div class="card-body">
+              <h5 class="card-title">${pokomonobj.name}</h5>
+              <p class="card-text">
+                ${pokomonobj.description}
+              </p>
+            </div>
+          </div>
+        
+      </div>`;
+  cardRow.appendChild(div);
+}
+
+console.log(cardRow);
+
+// fetch the name
+for (let pokomonobj of data) {
+  console.log(pokomonobj.name);
+}
+
 console.log(inputEl);
+
 // inputEl.focus();
 document.addEventListener("keypress", function (event) {
   if (event.key === "/") {
